a1e901d26bc68561932c188c750ae2778c78bec0
PETRUSBLACK <onukwupetrusoge@gmail.com>
2023-04-04 15:41:50 +0100

    ft/Petrus/aggregator&agent-proto

--- booosta_proto/py_grpc/personnel_pb2_grpc.py
+++ booosta_proto/py_grpc/personnel_pb2_grpc.py
@@ -2,7 +2,7 @@
 """Client and server classes corresponding to protobuf-defined services."""
 import grpc
 
-from booosta_proto.py_grpc import personnel_pb2 as personnel__pb2
+import personnel_pb2 as personnel__pb2
 
 
 class PersonnelServiceStub(object):
@@ -19,12 +19,34 @@ class PersonnelServiceStub(object):
                 request_serializer=personnel__pb2.GetRetailerByUserIdRequest.SerializeToString,
                 response_deserializer=personnel__pb2.GetRetailerByUserIdResponse.FromString,
                 )
+        self.GetAgentByUserId = channel.unary_unary(
+                '/personnel.PersonnelService/GetAgentByUserId',
+                request_serializer=personnel__pb2.GetAgentByUserIdRequest.SerializeToString,
+                response_deserializer=personnel__pb2.GetAgentByUserIdResponse.FromString,
+                )
+        self.GetAggregatorByUserId = channel.unary_unary(
+                '/personnel.PersonnelService/GetAggregatorByUserId',
+                request_serializer=personnel__pb2.GetAggregatorByUserIdRequest.SerializeToString,
+                response_deserializer=personnel__pb2.GetAggregatorByUserIdResponse.FromString,
+                )
 
 
 class PersonnelServiceServicer(object):
     """Missing associated documentation comment in .proto file."""
 
     def GetRetailerByUserId(self, request, context):
+        """Missing associated documentation comment in .proto file."""
+        context.set_code(grpc.StatusCode.UNIMPLEMENTED)
+        context.set_details('Method not implemented!')
+        raise NotImplementedError('Method not implemented!')
+
+    def GetAgentByUserId(self, request, context):
+        """Missing associated documentation comment in .proto file."""
+        context.set_code(grpc.StatusCode.UNIMPLEMENTED)
+        context.set_details('Method not implemented!')
+        raise NotImplementedError('Method not implemented!')
+
+    def GetAggregatorByUserId(self, request, context):
         """Missing associated documentation comment in .proto file."""
         context.set_code(grpc.StatusCode.UNIMPLEMENTED)
         context.set_details('Method not implemented!')
@@ -37,6 +59,16 @@ def add_PersonnelServiceServicer_to_server(servicer, server):
                     servicer.GetRetailerByUserId,
                     request_deserializer=personnel__pb2.GetRetailerByUserIdRequest.FromString,
                     response_serializer=personnel__pb2.GetRetailerByUserIdResponse.SerializeToString,
+            ),
+            'GetAgentByUserId': grpc.unary_unary_rpc_method_handler(
+                    servicer.GetAgentByUserId,
+                    request_deserializer=personnel__pb2.GetAgentByUserIdRequest.FromString,
+                    response_serializer=personnel__pb2.GetAgentByUserIdResponse.SerializeToString,
+            ),
+            'GetAggregatorByUserId': grpc.unary_unary_rpc_method_handler(
+                    servicer.GetAggregatorByUserId,
+                    request_deserializer=personnel__pb2.GetAggregatorByUserIdRequest.FromString,
+                    response_serializer=personnel__pb2.GetAggregatorByUserIdResponse.SerializeToString,
             ),
     }
     generic_handler = grpc.method_handlers_generic_handler(
@@ -62,5 +94,39 @@ class PersonnelService(object):
         return grpc.experimental.unary_unary(request, target, '/personnel.PersonnelService/GetRetailerByUserId',
             personnel__pb2.GetRetailerByUserIdRequest.SerializeToString,
             personnel__pb2.GetRetailerByUserIdResponse.FromString,
+            options, channel_credentials,
+            insecure, call_credentials, compression, wait_for_ready, timeout, metadata)
+
+    @staticmethod
+    def GetAgentByUserId(request,
+            target,
+            options=(),
+            channel_credentials=None,
+            call_credentials=None,
+            insecure=False,
+            compression=None,
+            wait_for_ready=None,
+            timeout=None,
+            metadata=None):
+        return grpc.experimental.unary_unary(request, target, '/personnel.PersonnelService/GetAgentByUserId',
+            personnel__pb2.GetAgentByUserIdRequest.SerializeToString,
+            personnel__pb2.GetAgentByUserIdResponse.FromString,
+            options, channel_credentials,
+            insecure, call_credentials, compression, wait_for_ready, timeout, metadata)
+
+    @staticmethod
+    def GetAggregatorByUserId(request,
+            target,
+            options=(),
+            channel_credentials=None,
+            call_credentials=None,
+            insecure=False,
+            compression=None,
+            wait_for_ready=None,
+            timeout=None,
+            metadata=None):
+        return grpc.experimental.unary_unary(request, target, '/personnel.PersonnelService/GetAggregatorByUserId',
+            personnel__pb2.GetAggregatorByUserIdRequest.SerializeToString,
+            personnel__pb2.GetAggregatorByUserIdResponse.FromString,
             options, channel_credentials,
             insecure, call_credentials, compression, wait_for_ready, timeout, metadata)
